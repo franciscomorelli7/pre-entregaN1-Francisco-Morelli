ef24d7a9fc3f8bd16257d27ed6bb5a5b13277cef
Francisco Morelli <pakimorelli7@gmail.com>
2023-07-12 22:46:11 -0400

    Nuevo carrito

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 let nombre = prompt("Ingrese su nombre")
+alert('Bienvenido a Mundo Cuervo' + " " + nombre)
 //Productos
 const camiseta = 'camiseta'
 const camperon = 'campera de abrigo'
@@ -7,9 +8,9 @@ const mediasLargas = 'medias de futbol'
 const soquetes = 'soquetes'
 const gorroDeLana = 'gorro de invierno'
 const gorra = 'gorra'
-let carrito = ""
+let carrito = " "
 function agregarProducto() {
-    let producto = prompt("Ingrese el nombre del producto:");
+    let producto = prompt("Ingrese el nombre del producto (En minuscula por favor): \n Camiseta \n Campera de abrigo \n Short deportivo \n Medias de futbol \n Soquetes \n Gorro de invierno \n Gorra");
     if(producto == camiseta || producto == camperon || producto == shortDeportivo || producto == mediasLargas || producto == soquetes || producto == gorroDeLana || producto == gorra){
     carrito += producto + "\n";
     alert("Se ha agregado el producto al carrito: " + producto);  
@@ -25,12 +26,12 @@ function mostrarCarrito() {
 while (true) {
     let opcion = prompt("Seleccione una opción:\n1. Agregar producto\n2. Mostrar carrito\n3. Salir");
   
-    if (opcion === "1") {
+    if (opcion == "1") {
       agregarProducto();
-    } else if (opcion === "2") {
+    } else if (opcion == "2") {
       mostrarCarrito();
-    } else if (opcion === "3") {
-      alert("¡Gracias por usar el carrito!");
+    } else if (opcion == "3") {
+      alert("¡Gracias por comprar en mundo cuervo!");
       break;
     } else {
      alert("Opción inválida. Por favor, seleccione una opción valida.");
